501d41db919ae2ebab945622008920de64c64465
ringge <57051648+ringge@users.noreply.github.com>
2023-04-16 10:33:49 +0700

    update

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -18,7 +18,8 @@ const config = {
     return [
       {
         // matching all API routes
-        source: "/api/:path*",
+        //source: "/api/:path*",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
